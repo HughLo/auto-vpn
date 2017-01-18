34e62760f6e66dd24db545fa4b523074120720e3
hugh <lorfalwit@gmail.com>
2017-01-18 23:30:15 +0800

    re-style codes and make it work on both linux and win

--- src/auto-vpn/main.go
+++ src/auto-vpn/main.go
@@ -1,108 +1,108 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "log"
-  "time"
-  "AWSCLI"
-  "ss"
+	"AWSCLI"
+	"fmt"
+	"log"
+	"os"
+	"ss"
+	"time"
 )
 
 //Replace this with your own EC2 Instance ID.
-var instance_id string = "i-ff6e595a"
+var instanceID = "i-xxxxx"
+
 //Replace this with your shadowsocks server password
-var psd_text string = "password"
+var psdText = "password"
+
 //Replace this with your own shadowsocks client port number
-var local_port int = 10801
+var localPort = 10801
 
 func main() {
-  sub_cmd := os.Args[1]
+	subCmd := os.Args[1]
 
-  cli_ctrl := AWSCLI.NewEC2Instance(instance_id)
+	cliCtrl := AWSCLI.NewEC2Instance(instanceID)
 
-  if cli_ctrl == nil {
-    log.Fatal("Cannot create AWS CLI instance")
-  }
+	if cliCtrl == nil {
+		log.Fatal("Cannot create AWS CLI instance")
+	}
 
-  if sub_cmd == "start" {
-    fmt.Printf("Starting AWS Instance %s\n", cli_ctrl.InstanceId)
+	if subCmd == "start" {
+		fmt.Printf("Starting AWS Instance %s\n", cliCtrl.InstanceId)
 
-    sr, err := cli_ctrl.StartInstance()
-    if err != nil {
-      fmt.Println("Start EC2 Instace Error")
-      log.Fatal(err)
-    }
+		sr, err := cliCtrl.StartInstance()
+		if err != nil {
+			fmt.Println("Start EC2 Instace Error")
+			log.Fatal(err)
+		}
 
-    fmt.Printf("current status:%s \n", sr.StartingInstances[0].CurrentState.Name)
+		fmt.Printf("current status:%s \n", sr.StartingInstances[0].CurrentState.Name)
 
-    cli_ctrl.WaitFor("running", time.Second * 60, func (err error, dr *AWSCLI.DescribeResult) {
-      if err == nil {
-        fmt.Printf("The instance %s running successfully \n", cli_ctrl.InstanceId)
-        ss_ctrl := SS.DefaultSSLocal()
-        if ss_ctrl == nil {
-          log.Fatal("Cannot create shadowsocks client instance. Do not forget to stop the EC instance.")
-        }
-        ss_ctrl.Password = psd_text
-        ss_ctrl.ServerAddr = dr.Reservations[0].Instances[0].PublicDnsName
-        ss_ctrl.LocalPort = local_port
-        err = ss_ctrl.StartDaemon()
-        if err != nil {
-          fmt.Println("Cannot start shadowsocks client. Do not forget to stop EC2 instance.")
-          log.Fatal(err)
-        } else {
-          fmt.Println("Start shadowsocks client successfully")
-        }
-      } else {
-        fmt.Println(err)
-      }
-    })
-  } else if sub_cmd == "stop" {
-    fmt.Printf("Stopping AWS Instance %s\n", cli_ctrl.InstanceId)
+		cliCtrl.WaitFor("running", time.Second*60, func(err error, dr *AWSCLI.DescribeResult) {
+			if err == nil {
+				fmt.Printf("The instance %s running successfully \n", cliCtrl.InstanceId)
+				ssCtrl := SS.New(dr.Reservations[0].Instances[0].PublicDnsName, "127.0.0.1", psdText, "aes-256-cfb", 8388, localPort, 600)
+				if ssCtrl == nil {
+					log.Fatal("Cannot create shadowsocks client instance. Do not forget to stop the EC instance.")
+				}
 
-    sr, err := cli_ctrl.StopInstance()
-    if err != nil {
-      fmt.Println("Stop EC2 Instace Error. Please try it later or stop it on AWS Offical Site.")
-      log.Fatal(err)
-    }
+				err = ssCtrl.Start()
+				if err != nil {
+					fmt.Println("Cannot start shadowsocks client. Do not forget to stop EC2 instance.")
+					log.Fatal(err)
+				} else {
+					fmt.Println("Start shadowsocks client successfully")
+				}
+			} else {
+				fmt.Println(err)
+			}
+		})
+	} else if subCmd == "stop" {
+		fmt.Printf("Stopping AWS Instance %s\n", cliCtrl.InstanceId)
 
-    fmt.Printf("current status: %s\n", sr.StoppingInstances[0].CurrentState.Name)
+		sr, err := cliCtrl.StopInstance()
+		if err != nil {
+			fmt.Println("Stop EC2 Instace Error. Please try it later or stop it on AWS Offical Site.")
+			log.Fatal(err)
+		}
 
-    cli_ctrl.WaitFor("stopped", time.Second * 60, func (err error, dr *AWSCLI.DescribeResult) {
-      if err == nil {
-        fmt.Printf("The instance %s stopped successfully \n", cli_ctrl.InstanceId)
-        ss_ctrl := SS.DefaultSSLocal()
-        if ss_ctrl == nil {
-          log.Fatal("Cannot create shadowsocks client instance.")
-        }
-        err = ss_ctrl.StopDaemon()
-        if err != nil {
-          log.Fatal(err)
-        } else {
-          fmt.Println("Stop shadowsocks client successfully")
-        }
-      } else {
-        log.Println(err)
-      }
-    })
-  } else if sub_cmd == "status" {
-    dr, err := cli_ctrl.InstanceState()
-    if err != nil {
-      log.Fatal(err)
-    }
+		fmt.Printf("current status: %s\n", sr.StoppingInstances[0].CurrentState.Name)
 
-    fmt.Printf("currnet status: %s\n", dr.Reservations[0].Instances[0].State.Name)
-    fmt.Printf("public dns name: %s\n", dr.Reservations[0].Instances[0].PublicDnsName)
-  } else if sub_cmd == "stop-dae" {
-    ss_ctrl := SS.DefaultSSLocal()
-    if ss_ctrl == nil {
-      log.Fatal("Cannot create shadowsocks client instance")
-    }
-    err := ss_ctrl.StopDaemon()
-    if err != nil {
-      log.Fatal(err)
-    } else {
-      fmt.Println("Stop shadowsocks client successfully")
-    }
-  }
+		cliCtrl.WaitFor("stopped", time.Second*60, func(err error, dr *AWSCLI.DescribeResult) {
+			if err == nil {
+				fmt.Printf("The instance %s stopped successfully \n", cliCtrl.InstanceId)
+				ssCtrl := SS.DefaultLocal()
+				if ssCtrl == nil {
+					log.Fatal("Cannot create shadowsocks client instance.")
+				}
+				err = ssCtrl.Stop()
+				if err != nil {
+					log.Fatal(err)
+				} else {
+					fmt.Println("Stop shadowsocks client successfully")
+				}
+			} else {
+				log.Println(err)
+			}
+		})
+	} else if subCmd == "status" {
+		dr, err := cliCtrl.InstanceState()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("currnet status: %s\n", dr.Reservations[0].Instances[0].State.Name)
+		fmt.Printf("public dns name: %s\n", dr.Reservations[0].Instances[0].PublicDnsName)
+	} else if subCmd == "stop-dae" {
+		ssCtrl := SS.DefaultLocal()
+		if ssCtrl == nil {
+			log.Fatal("Cannot create shadowsocks client instance")
+		}
+		err := ssCtrl.Stop()
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			fmt.Println("Stop shadowsocks client successfully")
+		}
+	}
 }
--- src/ss/ss.go
+++ src/ss/ss.go
@@ -3,79 +3,167 @@
 package SS
 
 import (
-  "os/exec"
-  "strconv"
+	"os/exec"
+	"runtime"
+	"strconv"
 )
 
-//SSLocal represents a shadowsocks client.
-type SSLocal struct {
-  ServerAddr string
-  ServerPort int
-  LocalAddr string
-  LocalPort int
-  Password string
-  Timeout int
-  Method string
+type controlSslocalDaemon interface {
+	Control(string) error
 }
 
-//DefaultSSLocal creates a new SSLocal with default configuration.
-func DefaultSSLocal() *SSLocal {
-  return &SSLocal {
-    ServerAddr: "",
-    ServerPort: 8388,
-    LocalAddr: "127.0.0.1",
-    LocalPort: 1088,
-    Password: "",
-    Timeout: 600,
-    Method: "aes-256-cfb",
-  }
+//Local represents a shadowsocks client.
+type Local struct {
+	ServerAddr string
+	ServerPort int
+	LocalAddr  string
+	LocalPort  int
+	Password   string
+	Timeout    int
+	Method     string
+}
+
+//LocalControl is the interface to control the shadowsocks client
+type LocalControl interface {
+	Start() error
+	Stop() error
+	Restart() error
+}
+
+//WinLocal represents a shadowsocks client in windows platform.
+type WinLocal struct {
+	Local
+}
+
+//LinuxLocal represents a shadowsocks client in linux platform.
+type LinuxLocal struct {
+	Local
+}
+
+//Control sslocal in windows platform
+func (wlocal *WinLocal) control(dae string) error {
+	args := []string{
+		"-s", wlocal.ServerAddr,
+		"-p", strconv.Itoa(wlocal.ServerPort),
+		"-b", wlocal.LocalAddr,
+		"-l", strconv.Itoa(wlocal.LocalPort),
+		"-k", wlocal.Password,
+		"-m", wlocal.Method,
+		"-t", strconv.Itoa(wlocal.Timeout),
+		"-d", dae,
+	}
+
+	cmd := exec.Command("sslocal", args...)
+	_, err := cmd.Output()
+	return err
+}
+
+//Start the shadowsocks client
+func (wlocal *WinLocal) Start() error {
+	return wlocal.control("start")
+}
+
+//Stop the shadowsocks client
+func (wlocal *WinLocal) Stop() error {
+	return wlocal.control("stop")
+}
+
+//Restart the shadowsocks client
+func (wlocal *WinLocal) Restart() error {
+	return wlocal.control("restart")
+}
+
+//Control sslocal in windows platform
+func (llocal *LinuxLocal) control(dae string) error {
+	args := []string{
+		"sslocal",
+		"-s", llocal.ServerAddr,
+		"-p", strconv.Itoa(llocal.ServerPort),
+		"-b", llocal.LocalAddr,
+		"-l", strconv.Itoa(llocal.LocalPort),
+		"-k", llocal.Password,
+		"-m", llocal.Method,
+		"-t", strconv.Itoa(llocal.Timeout),
+		"-d", dae,
+	}
+
+	cmd := exec.Command("sudo", args...)
+	_, err := cmd.Output()
+	return err
+}
+
+//Start the shadowsocks client
+func (llocal *LinuxLocal) Start() error {
+	return llocal.control("start")
+}
+
+//Stop the shadowsocks client
+func (llocal *LinuxLocal) Stop() error {
+	return llocal.control("stop")
+}
+
+//Restart the shadowsocks client
+func (llocal *LinuxLocal) Restart() error {
+	return llocal.control("restart")
+}
+
+//DefaultLocal creates a new Local with default configuration.
+func DefaultLocal() LocalControl {
+	local := Local{
+		ServerAddr: "",
+		ServerPort: 8388,
+		LocalAddr:  "127.0.0.1",
+		LocalPort:  1088,
+		Password:   "",
+		Timeout:    600,
+		Method:     "aes-256-cfb",
+	}
+
+	var localControl LocalControl
+
+	switch runtime.GOOS {
+	case "windows":
+		winLocal := new(WinLocal)
+		winLocal.Local = local
+		localControl = winLocal
+
+	case "linux":
+		linuxLocal := new(LinuxLocal)
+		linuxLocal.Local = local
+		localControl = linuxLocal
+	}
+
+	return localControl
 }
 
 //New creates a new SSLocal. srv is server address, local is local address, psd
 //is the shadowsocks server password, local_port is the port number of shadowsocks
 //client, time_out is the time out setup of shadowsocks client.
-func New(srv, local, psd, method string,
-  srv_port, local_port, time_out int) *SSLocal {
-  return &SSLocal{
-    ServerAddr: srv,
-    ServerPort: srv_port,
-    LocalAddr: local,
-    LocalPort: local_port,
-    Password: psd,
-    Timeout: time_out,
-    Method: method,
-  }
-}
+func New(srv, localAddr, psd, method string,
+	srvPort, localPort, timeOut int) LocalControl {
+	local := Local{
+		ServerAddr: srv,
+		ServerPort: srvPort,
+		LocalAddr:  localAddr,
+		LocalPort:  localPort,
+		Password:   psd,
+		Timeout:    timeOut,
+		Method:     method,
+	}
 
-//StartDaemon starts sslocal as daemon
-func (self *SSLocal) StartDaemon() error {
-  return self.control_sslocal_daemon("start")
-}
+	var localControl LocalControl
 
-//StopDaemon stops sslocal daemon
-func (self *SSLocal) StopDaemon() error {
-  return self.control_sslocal_daemon("stop")
-}
+	switch runtime.GOOS {
+	case "windows":
+		winLocal := new(WinLocal)
+		winLocal.Local = local
+		localControl = winLocal
 
-//RestartDaemon restarts sslocal daemon
-func (self *SSLocal) RestartDaemon() error {
-  return self.control_sslocal_daemon("restart")
-}
+	case "linux":
+		linuxLocal := new(LinuxLocal)
+		linuxLocal.Local = local
+		localControl = linuxLocal
+	}
 
-func (self *SSLocal) control_sslocal_daemon(dae string) error {
-  args := []string {
-    "sslocal",
-    "-s", self.ServerAddr,
-    "-p", strconv.Itoa(self.ServerPort),
-    "-b", self.LocalAddr,
-    "-l", strconv.Itoa(self.LocalPort),
-    "-k", self.Password,
-    "-m", self.Method,
-    "-t", strconv.Itoa(self.Timeout),
-    "-d", dae,
-  }
-
-  cmd := exec.Command("sudo", args...)
-  _, err := cmd.Output()
-  return err
+	return localControl
 }
